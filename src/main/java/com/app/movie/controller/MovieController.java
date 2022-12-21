8b7a49d7acffa7647e45d4a57eaa7af75ccf30e6
Mateo Cortes <mateo.cortes1@o365.unab.edu.co>
2022-12-20 20:06:55 -0500

    feat: add endpoint to get a movie by its id

--- src/main/java/com/app/movie/controller/MovieController.java
+++ src/main/java/com/app/movie/controller/MovieController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/movie")
 @CrossOrigin(origins = "*")
@@ -18,6 +20,11 @@ public class MovieController {
     @GetMapping("")
     public Iterable<Movie> get() {
         return service.get();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Movie> getById(@PathVariable("id") String id) {
+        return service.getById(id);
     }
 
     @PostMapping("")
